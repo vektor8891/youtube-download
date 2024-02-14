69fc952b2fbe735f782198f091de2dcea78f4ed6
Viktor Szabo <vszabo@protonmail.com>
2024-02-13 22:34:14 -0500

    Update URL path and add little_bear.txt

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 from lib.download_video import download_video
 from lib.get_channel_videos import get_channel_videos
 
-url_path = 'output/url.txt'
+url_path = 'output/little_bear.txt'
 channel_id = 'UCncFzSeWqySvOXEbQoEJsZg'
 overwrite = False
-
 
 def main():
     """
@@ -17,11 +16,11 @@ def main():
         os.remove(url_path)
     if not os.path.exists(url_path):
         get_channel_videos(channel_id, url_path)
-    with open(url_path, 'r') as f:
-        for line in f:
-            print(f'Downloading {line.strip()}')
-            url = line.strip()
-            download_video(url, 'output')
+    # with open(url_path, 'r') as f:
+    #     for line in f:
+    #         print(f'Downloading {line.strip()}')
+    #         url = line.strip()
+    #         download_video(url, 'output')
 
 
 if __name__ == "__main__":
