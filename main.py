d98acd673d462b0dc6aab23e108a2719d48f2358
Viktor Szabo <vszabo@protonmail.com>
2024-02-13 23:03:24 -0500

    Add video download functionality

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import os
+import itertools
 from lib.download_video import download_video
 from lib.get_channel_videos import get_channel_videos
 
+# Little Bear
 url_path = 'output/little_bear.txt'
 channel_id = 'UCncFzSeWqySvOXEbQoEJsZg'
+# Bogyo es Baboca
+# url_path = 'output/bogyo_es_baboca.txt'
+# channel_id = 'UCmsxuZrnb-MNYpqPd-XxLog'
+# Wall-E
+# url_path = 'output/wall_e.txt'
+# channel_id = 'UCGh0buCqdEGegoIkU8ZVxuQ'
+max_videos = 1
 overwrite = False
+
 
 def main():
     """
@@ -16,11 +26,11 @@ def main():
         os.remove(url_path)
     if not os.path.exists(url_path):
         get_channel_videos(channel_id, url_path)
-    # with open(url_path, 'r') as f:
-    #     for line in f:
-    #         print(f'Downloading {line.strip()}')
-    #         url = line.strip()
-    #         download_video(url, 'output')
+    with open(url_path, 'r') as f:
+        for line in itertools.islice(f, max_videos):
+            print(f'Downloading {line.strip()}')
+            url = line.strip()
+            download_video(url, 'output')
 
 
 if __name__ == "__main__":
